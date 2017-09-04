34061b5d06ba658e2a4921e3554cfc652c3b5d94
xiangxiaobaog3 <xiangxiaobaog3@gmail.com>
2017-09-04 19:39:44 +0800

    增加了view login和logout逻辑部分，添加了session功能

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.contrib import auth
+from django.http import JsonResponse
 # Create your views here.
+
+class login(request):
+    if request.method == "POST":
+
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        ret = {"status": 0, "errmsg": ""}
+        if user:
+            return HttpResponseRedirect('/assets/')
+        else:
+            ret["status"] = 1
+            ret["errmsg"] = "用户名和密码错误，请联系管理员."
+            return JsonResponse(ret)
+    else:
+        return HttpResponseRedirect('/assets/')
+
--- django_test1/views.py
+++ django_test1/views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import render
+# coding: utf-8
+from django.shortcuts import render, redirect
 from django.shortcuts import render_to_response
+from django.contrib import auth
+from django.http import JsonResponse
 # Create your views here.
 
 def login(request):
+    if request.method == "POST":
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        # if username == 'xiangxiaobaog3@gmail.com' and password == 'bao982':
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            request.session['username'] = username
+            return redirect('/assets/')
+        else:
+            return render_to_response('login.html')
+    else:
+        return render_to_response('login.html')
 
+
+def logout(request):
     return render_to_response("login.html")
 
 
 def base(request):
-
     return render_to_response("base.html")
 
 def assets(request):
+    user_dict = request.session.get('username', None)
+    if user_dict:
+        return render_to_response("assets.html", {'username': user_dict})
+    else:
+        return redirect('/login/')
 
-    return render_to_response("assets.html")
