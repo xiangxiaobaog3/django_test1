9cbbf4356b142c9fbffb069c44d12d06d230d1a4
xiangxiaobaog3 <xiangxiaobaog3@gmail.com>
2017-09-19 20:51:23 +0800

    新增了DateTimeField字段，创建自动添加和修改后自动更新日期

--- account/models.py
+++ account/models.py
@@ -4,21 +4,20 @@ from account.myauth import UserProfile
 # Create your models here.
 
 class UserType(models.Model):
-	
-	name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
 
 class Group(models.Model):
-	
-	Name = models.CharField(max_length=50)
+    Name = models.CharField(max_length=50)
 	
 	
 class UserInfo(models.Model):
-	
-	username = models.CharField(max_length=50)
-	
-	password = models.CharField(max_length=50)
-	
-	typeId = models.ForeignKey(UserType, verbose_name='用户类型')
-	
-	group_relation = models.ManyToManyField(Group, verbose_name='用户所数组')
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    typeId = models.ForeignKey(UserType, verbose_name='用户类型',null=True)
+    group_relation = models.ManyToManyField(Group, verbose_name='用户所数组')
+    crtime = models.DateTimeField(auto_now=True)
+    uptime = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.username
